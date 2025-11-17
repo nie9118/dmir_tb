7806d4269b3b24b87b487afab2885af097043bb5
shengbin <2714971505@qq.com>
2025-11-17 15:29:10 +0800

    print

--- nsb/TimeBridge-v1-norandom/experiments/exp_long_term_forecasting.py
+++ nsb/TimeBridge-v1-norandom/experiments/exp_long_term_forecasting.py
@@ -162,13 +162,13 @@ class Exp_Long_Term_Forecast(Exp_Basic):
 
                 train_loss.append(loss.item())
 
-                if (i + 1) % 100 == 0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
-                    speed = (time.time() - time_now) / iter_count
-                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
-                    iter_count = 0
-                    time_now = time.time()
+                # if (i + 1) % 100 == 0:
+                #     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                #     speed = (time.time() - time_now) / iter_count
+                #     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                #     print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                #     iter_count = 0
+                #     time_now = time.time()
 
                 loss.backward()
                 model_optim.step()
