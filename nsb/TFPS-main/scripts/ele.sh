a29623ed5c923d3aca5595ed8331191c06cce5c8
shengbin <2714971505@qq.com>
2025-12-22 01:54:18 +0800

    1

---
+++ nsb/TFPS-main/scripts/ele.sh
@@ -0,0 +1,76 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+# seq_len=96
+model_name=PatchTST_MoE_cluster
+
+root_path_name=./dataset/electricity/
+data_path_name=electricity.csv
+model_id_name=electricity
+data_name=custom
+
+random_seed=2021
+for seq_len in 720
+do
+for pred_len in 96 192 336 720
+do
+for random_seed in 2021
+do
+for learning_rate in 0.001 0.0001 0.0005 0.001 0.005 0.01 0.05
+do
+for T_num_expert in 1 2 4 8
+do
+for T_top_k in 1 2 4 8
+do
+for F_num_expert in 1 2 4 8
+do
+for F_top_k in 1 2 4 8
+do
+    python -u run_longExp.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 321 \
+      --e_layers 3 \
+      --n_heads 32 \
+      --d_model 512 \
+      --d_ff 512 \
+      --dropout 0.2\
+      --fc_dropout 0.2\
+      --head_dropout 0\
+      --patch_len 16\
+      --stride 8\
+      --T_num_expert $T_num_expert\
+      --T_top_k $T_top_k\
+      --F_num_expert $F_num_expert\
+      --F_top_k $F_top_k\
+      --beta 0.1 \
+      --des 'Exp' \
+      --train_epochs 100\
+      --patience 20\
+      --lradj 'TST'\
+      --pct_start 0.4\
+      --use_multi_gpu \
+      --devices 0,1,2,3 \
+      --itr 1 --batch_size 128 --learning_rate $learning_rate >logs/LongForecasting/ettm1/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len'_'$T_num_expert'_'$T_top_k'_'$F_num_expert'_'$F_top_k'_'$learning_rate'_0.1'.log
+done
+done
+done
+done
+done
+done
+done
+done
