400aca246563fd39055ed496c82eda7c363de750
shengbin <2714971505@qq.com>
2025-12-26 02:11:52 +0800

    1

---
+++ job_tra.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=tfps
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH -p long
+#SBATCH -q gpu-12
+#SBATCH --time=3-00:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --mem=230G
+
+source activate TFPS
+cd /home/guangyi.chen/causal_group/zijian/dmir_tb/tfps_1/scripts
+./traffic.sh
